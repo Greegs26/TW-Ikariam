70b8d5bcd324a4386e293e25236cdfe2d5c4fcb4
Greegs26 <greganchristian@gmail.com>
2024-06-10 23:09:24 -0300

    extension script to be used with pillaging script

---
+++ coordsFinder.py
@@ -0,0 +1,19 @@
+#trying to create my own Piracy bot using PyAutoGUI
+import pyautogui
+import time
+import random
+import sys
+
+time.sleep(5)
+# Get the current mouse cursor's X and Y positions
+x, y = pyautogui.position()
+
+    # Print the coordinates
+print(f"The current mouse position is X: {x} Y: {y}")
+    # -602, 451
+    # -1692, 535
+    # -774, 485
+    # - 775, 530
+    # - 775, 574
+    # -1129, 652
+time.sleep(2)
