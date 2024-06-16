5221cc46fee7a6a2fb580bd92d6d41a1b7d423e1
Greegs26 <greganchristian@gmail.com>
2024-06-16 15:03:59 -0300

    updated with new coords

--- coordsFinder.py
+++ coordsFinder.py
@@ -6,14 +6,17 @@ import sys
 
 time.sleep(5)
 # Get the current mouse cursor's X and Y positions
-x, y = pyautogui.position()
+while True:
+    x, y = pyautogui.position()
 
     # Print the coordinates
-print(f"The current mouse position is X: {x} Y: {y}")
+    print(f"The current mouse position is X: {x} Y: {y}")
     # -602, 451
     # -1692, 535
     # -774, 485
     # - 775, 530
     # - 775, 574
     # -1129, 652
-time.sleep(2)
+
+    #X: -616 Y: 724
+    time.sleep(2)
