9d290966a93f969742d3ff55726e083dafc4da03
Greegs26 <greganchristian@gmail.com>
2024-06-17 21:47:11 -0300

    added 5 more coords to cycle through

--- PillagingScript.py
+++ PillagingScript.py
@@ -4,50 +4,61 @@ import time
 import random
 import sys
 
-coords=[(-606, 331), (-1009, 283), (-1158, 362), (-1276, 421), (-1465, 512), (-1506, 708), (-1191, 726)]
+#The current mouse position is X: -321 Y: 460
+#The current mouse position is X: -543 Y: 433
+#The current mouse position is X: -625 Y: 483
+#The current mouse position is X: -686 Y: 515
+#The current mouse position is X: -794 Y: 563
+#The current mouse position is X: -816 Y: 672
+#The current mouse position is X: -641 Y: 682
+
+#The current mouse position is X: -360 Y: 691
+#The current mouse position is X: -269 Y: 668
+#The current mouse position is X: -271 Y: 599
+#The current mouse position is X: -386 Y: 540
+#The current mouse position is X: -284 Y: 543
+coords=[(-321, 460), (-543, 433), (-625, 483), (-686, 515), (-794, 563), (-816, 672), (-641, 682), (-360, 961), (-269, 668), (-271, 599), (-386, 540), (-284, 543)]
+index=0
 
 # Wait for 5 seconds to set up proper browser tab before starting
 time.sleep(5)
 
 while True: 
-    #The current mouse position is X: -606 Y: 331
-    #The current mouse position is X: -1009 Y: 283
-    #The current mouse position is X: -1158 Y: 362
-    #The current mouse position is X: -1276 Y: 421
-    #The current mouse position is X: -1465 Y: 512
-    #The current mouse position is X: -1506 Y: 708
-    #The current mouse position is X: -1191 Y: 726
+
     #This is used to click on actual city
     coord = coords[index]
     pyautogui.click(coord[0], coord[1])
     time.sleep(2)
+
     #used to click on pillage button
-    pyautogui.click(-616, 724)
-
-
+    pyautogui.click(-1692, 511)
     time.sleep(2)
-    pyautogui.click(-1692, 535)
+    #used to click on spearmen
+    pyautogui.click(-123, 532)
     time.sleep(2)
-    pyautogui.click(-288, 484)
+    #Selecting spearmen, Type the number 7
+    pyautogui.write('7', interval=0.1)
     time.sleep(2)
-    #pyautogui.click(-775, 530)
-    #time.sleep(2)
-    pyautogui.click(-287, 573)
+    #Sending max mortars
+    pyautogui.click(-288, 577)
     time.sleep(2)
-
-    pyautogui.click(-646, 651)
+    #Selecting boats
+    pyautogui.click(-643, 657)
     time.sleep(2)
     #Selecting boats, Type the number 5000
     pyautogui.write('5000', interval=0.1)
     time.sleep(2)
+    #Send pillage
+    pyautogui.click(-159, 650)
+    time.sleep(2)
+    #Select Military Advisor
+    pyautogui.click(-256, 159)
+    time.sleep(2)
+    #Exit Military Advisor
+    pyautogui.click(-85,253)
+    time.sleep(2)
 
-    # -645, 653
-    pyautogui.click(-159, 656)
-    time.sleep(2)
-    pyautogui.click(-254, 165)
-    time.sleep(2)
-    pyautogui.click(-85, 251)
-    time.sleep(2)
+
     # Wait for 150 seconds before starting again
     print("Begin Pillage - wait time of 1h50mins")
     time.sleep(6600)
