3f40f23f435f55b79b9983d26d04a0b2b4992f53
Greegs26 <greganchristian@gmail.com>
2024-12-16 21:01:26 -0400

    remove feature auto-closing the browser

--- SeleniumIkariamScript.py
+++ SeleniumIkariamScript.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
 import time
 
-# Initialize the Chrome WebDriver
-driver = webdriver.Chrome()
+# Initialize the Firefox WebDriver
+driver = webdriver.Firefox()
 
 # Open Ikariam
 driver.get("https://www.ikariam.org")
 driver.maximize_window()
 print("Ikariam browser opened!")
 
-time.sleep(60) # wait for 60 seconds before continuing with scriot
+time.sleep(60) # wait for 60 seconds before continuing with script
 
 # Keep the browser open until user presses Enter
 input("Press Enter to close the browser")
