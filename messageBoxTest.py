1f867438d0026332e21bed55090856910dd7d058
Greegs26 <greganchristian@gmail.com>
2024-12-16 20:36:23 -0400

    adding choice option

--- messageBoxTest.py
+++ messageBoxTest.py
@@ -1,3 +1,7 @@
 import pyautogui
 
 pyautogui.alert("Test message.", "Alert Title")
+
+choice = pyautogui.confirm("Are you sure?", "Confirmation", ["Yes", "No"])
+if choice == "Yes":
+    print("User confirmed")
