b3a853ab284110c148e80b9773de79dda6d9c066
Greegs26 <greganchristian@gmail.com>
2025-02-09 13:50:58 -0400

    Calculate distance between 2x islands using formula found in Ikariam wiki

---
+++ DistanceCalculator.py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_travel_time(x1, y1, x2, y2):
+    return 20 * math.sqrt((x2-x1) ** 2 + (y2-y1) ** 2)
+                          
+def main():
+    print("Enter the first coordinate (x1 y1):")
+    x1, y1 = map(float, input().split())
+
+    print("Enter the second coordinate (x2 y2):")
+    x2, y2 = map(float, input().split())
+
+    travel_time = calculate_travel_time(x1, y1, x2, y2)
+    print("Travel time is "+str(travel_time))
+
+if __name__ == "__main__":
+    main()
